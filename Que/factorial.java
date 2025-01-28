90c5ca28c5bd4f29346d3b547b94bce69f0dd571
karankose <karankose13@gmail.com>
2025-01-28 15:23:47 +0530

    day1

---
+++ Que/factorial.java
@@ -0,0 +1,55 @@
+class factorial{
+    public static void main(String[] args)
+    {
+        int x = 0;
+       for (int i = 1; i<=10;i++){
+            x += i;
+       } 
+       System.out.println(x);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class factorial{
+//     public static void main(String arg[]){
+//         System.out.println(126 % 10);
+//         // to saperate last no division bu 10
+//         System.out.println(126  /  10);
+//     }
+// }
+
+// The modulus operator gives the remainder of the division.
+// Dividing 126 by 10 gives a remainder of 6.
+// So, the output of this line will be: 6.
+// 126 / 10:
+
+// Integer division divides the number and discards any remainder (since integers are involved).
+// Dividing 126 by 10 gives 12.6, but only the integer part (12) is kept.
+// So, the output of this line will be: 12.
